706a8d4b23b62718953f44ea1965151fcb3423cb
Brooke Brown <brookejb@umich.edu>
2026-03-29 14:52:45 -0400

    Add debug output to chat response to diagnose memory

--- api/chat.js
+++ api/chat.js
@@ -103,7 +103,7 @@ export default async function handler(req, res) {
       }
     }
 
-    res.json({ reply: cleanReply });
+    res.json({ reply: cleanReply, _debug: { memoryFound: !!memoryMatch, rawReply } });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
